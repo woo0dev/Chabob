d007435399101155ff2e684d08947cda4e8560be
woo0 <57060443+woo0dev@users.noreply.github.com>
2023-06-27 00:10:08 +0900

    Create Chabob Project

---
+++ Chabob/Chabob/ChabobApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChabobApp.swift
+//  Chabob
+//
+//  Created by woo0 on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ChabobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
